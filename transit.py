8c42afc32cbb160df50d6d8d656786e6b1378141
smohapatraa <smohapatra@gmail.com>
2019-10-28 05:56:19 +0530

    second version

--- transit.py
+++ transit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as nm
+print("hello")
 honda = pd.read_excel('C:\\Users\\Administrator\\Desktop\\s07\\transit\\output.xlsx',0)
 honda1=honda.loc[:,['Frame #','Engine No','Model Name','Color Code','Model Variant','Color','Physical Status','Product Name','Selling Dealer','Plant Code','Transporter Code','Transporter Name','Dealer Code','HMSI Invoice Amount','HMSI Load Reference No','Truck Number','Purchase Order No.','Payment Amount','Dispatch Date','Model Code','Manufacturing Date','SAP Invoice Number', 'HSN Code','Reference Number']]
 honda2=honda.loc[:,['SAP Invoice Number','Frame #','Model Name','Color Code','Model Variant','Color','HMSI Invoice Amount','Truck Number','Dispatch Date','SAP Invoice Number','HMSI Load Reference No','SAP Invoice Number','Transporter Name']]
